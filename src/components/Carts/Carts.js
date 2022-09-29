1e6f86ca3f7540932ce0c31dc91d5d24ae6f552a
sabikonnahar.sr <sabikonnahar.sr@gmail.com>
2022-09-29 21:25:27 +0600

    added background color

--- src/components/Carts/Carts.js
+++ src/components/Carts/Carts.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './Carts.css'
 
-const Cart = ({handleAddToCart,cart}) => {
+const Cart = ({cart, handleAddToCart}) => {
    
-    const {img, name, discription, age, time} = cart;
+    const {img, name, description, age, time} = cart;
     return (
         <div className='cart'>
            <img src={img} alt="" />
            <div className='cart-info'>
                 <h2 className='cart-name'>{name}</h2>
-                <p> About : {discription}</p>
+                <p> About: {description}</p>
                 <div className='p-text'>
                     <p>time required: {time}</p>
                     <p>age: {age} </p>
                 </div>
            </div>
-           <button onClick={()=>  handleAddToCart(cart)} className='btn-cart'>
+           <button onClick={()=> handleAddToCart(cart)} className='btn-cart'>
                 <p className='btn-text'>Add To Cart</p> 
             </button>
         </div>
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -7,34 +7,36 @@ const Shop = () => {
     const [carts, setCarts] = useState([]);
     const [sidebar, setSidebar] = useState([]);
 
+
     useEffect(() => {
         fetch('carts.json')
         .then(res => res.json())
         .then(data => setCarts(data))
     }, []);
     const handleAddToCart = (carts) => {
-        console.log(carts)
         const newCart = [...carts, carts]
         setCarts(newCart);
     }
     return (
        <div className='shop-container'>
-           <div className='carts-container'>
-              <h2>Select today’s exercise</h2>    
-           </div>
+              <h2 className='select-title'>Select today’s exercise</h2>    
 
-        <div className='sidebar-container'>         
-            <Sidebar sidebar={sidebar}></Sidebar>
-
-        </div>
+           <div className='mainTwoContainer'>
 
            <div className='carts-container'> 
            {
-              carts.map(cart=> <Cart key={cart.id}
+              carts.map(cart=> <Cart 
+                key={cart.id}
                 cart= {cart}
                 handleAddToCart = {handleAddToCart}
                 ></Cart>)
               }
+           </div>
+
+           <div className='sidebar-container'>         
+            <Sidebar sidebar={sidebar}></Sidebar>
+            </div>
+            
            </div>
             
        </div>   
