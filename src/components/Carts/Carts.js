39c35849d0441d74b99fbe92f2a115eba234a96b
sabikonnahar.sr <sabikonnahar.sr@gmail.com>
2022-09-29 14:27:24 +0600

    added btn background color

--- src/components/Carts/Carts.js
+++ src/components/Carts/Carts.js
@@ -2,18 +2,19 @@ import React from 'react';
 import './Carts.css'
 
 const Cart = ({handleAddToCart,cart}) => {
-    const {img, name, age, time} = cart;
+    const {img, name, discription, age, time} = cart;
     return (
         <div className='cart'>
            <img src={img} alt="" />
            <div className='cart-info'>
                 <h2 className='cart-name'>{name}</h2>
+                <p> About : {discription}</p>
                 <h4>time required: {time}</h4>
                 <p>age: {age} </p>
-                <button onClick={()=>  handleAddToCart()} className='btn-cart'>
-                <p className='btn-text'>Add To Cart</p> 
-                </button>
            </div>
+           <button onClick={()=>  handleAddToCart()} className='btn-cart'>
+                <p className='btn-text'>Add To Cart</p> 
+            </button>
         </div>
     );
 };
